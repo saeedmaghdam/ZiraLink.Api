4758f42251aa60baf24f3a0f549f3ceb7128e09e
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-08-25 15:52:14 +0330

    Corrected lines format.

--- src/ZiraLink.Api/DependencyResolver.cs
+++ src/ZiraLink.Api/DependencyResolver.cs
@@ -30,125 +30,125 @@ public static class DependencyResolver
         services.AddScoped<ICustomerService, CustomerService>();
         services.AddSingleton<ITokenService, TokenService>();
         services.AddSingleton<IBus, Bus>();
-// Add services to the container.
-services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigins",
-        builder =>
+        // Add services to the container.
+        services.AddCors(options =>
         {
-            builder.WithOrigins(configuration["ZIRALINK_WEB_URL"]!)
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials()
-                .Build();
+            options.AddPolicy("AllowSpecificOrigins",
+                builder =>
+                {
+                    builder.WithOrigins(configuration["ZIRALINK_WEB_URL"]!)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .Build();
+                });
         });
-});
 
-var connectionString = configuration["ASPNETCORE_ENVIRONMENT"] == "Development" ? $"Data Source={Path.Combine(pathToExe, "database.db")}" : configuration["ZIRALINK_CONNECTIONSTRINGS_DB"];
-services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
-//services.AddDbContext<AppDbContext>();
+        var connectionString = configuration["ASPNETCORE_ENVIRONMENT"] == "Development" ? $"Data Source={Path.Combine(pathToExe, "database.db")}" : configuration["ZIRALINK_CONNECTIONSTRINGS_DB"];
+        services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+        //services.AddDbContext<AppDbContext>();
 
-var connectionMultiplexer = ConnectionMultiplexer.Connect(new ConfigurationOptions
-{
-    EndPoints = { configuration["ZIRALINK_CONNECTIONSTRINGS_REDIS"]! },
-    AbortOnConnectFail = false,
-    Password = configuration["ZIRALINK_REDIS_PASSWORD"]!
-});
-services.AddSingleton<IConnectionMultiplexer>(sp => connectionMultiplexer);
-
-services.AddAuthorization();
-services
-    .AddBff()
-    .AddRemoteApis();
-JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
-services.AddAuthentication(options =>
-{
-    //options.DefaultScheme = "Cookies";
-    //options.DefaultChallengeScheme = "oidc";
-    //options.DefaultSignOutScheme = "oidc";
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = "oidc";
-    options.DefaultSignOutScheme = "oidc";
-})
-            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
-            .AddJwtBearer("Bearer", options =>
-            {
-                options.RequireHttpsMetadata = false;
-                options.Authority = configuration["ZIRALINK_URL_IDS"]!;
-                options.SaveToken = true;
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateAudience = false
-                };
-
-                var db = connectionMultiplexer.GetDatabase(10);
-
-                options.Events = new JwtBearerEvents
-                {
-                    OnMessageReceived = async context =>
-                    {
-                        var tokenp = context.Request.Headers["Authorization"];
-                        tokenp = tokenp.ToString().Replace("Bearer ", "");
-                        var token = await db.StringGetAsync($"tokenptoken:{tokenp}");
-                        context.Token = token;
-                    }
-                };
-            })
-            .AddOpenIdConnect("oidc", options =>
-            {
-                options.RequireHttpsMetadata = false;
-                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.Authority = configuration["ZIRALINK_URL_IDS"]!;
-                options.ClientId = "bff";
-                options.ClientSecret = "secret";
-                options.ResponseType = OidcConstants.ResponseTypes.Code;
-                options.Scope.Clear();
-                options.Scope.Add("ziralink");
-                options.Scope.Add("openid");
-                options.Scope.Add("profile");
-                options.Scope.Add("email");
-                options.SaveTokens = true;
-
-                options.GetClaimsFromUserInfoEndpoint = true;
-
-                if (configuration["ASPNETCORE_ENVIRONMENT"] != "Production")
-                {
-                    HttpClientHandler handler = new HttpClientHandler();
-                    //handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-                    options.BackchannelHttpHandler = handler;
-                }
-            });
-
-services.AddHttpContextAccessor();
-
-services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-services.AddEndpointsApiExplorer();
-services.AddSwaggerGen(setup =>
-{
-    var jwtSecurityScheme = new OpenApiSecurityScheme
-    {
-        BearerFormat = "JWT",
-        Name = "JWT Authentication",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = JwtBearerDefaults.AuthenticationScheme,
-        Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
-
-        Reference = new OpenApiReference
+        var connectionMultiplexer = ConnectionMultiplexer.Connect(new ConfigurationOptions
         {
-            Id = JwtBearerDefaults.AuthenticationScheme,
-            Type = ReferenceType.SecurityScheme
-        }
-    };
+            EndPoints = { configuration["ZIRALINK_CONNECTIONSTRINGS_REDIS"]! },
+            AbortOnConnectFail = false,
+            Password = configuration["ZIRALINK_REDIS_PASSWORD"]!
+        });
+        services.AddSingleton<IConnectionMultiplexer>(sp => connectionMultiplexer);
 
-    setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+        services.AddAuthorization();
+        services
+            .AddBff()
+            .AddRemoteApis();
+        JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+        services.AddAuthentication(options =>
+        {
+            //options.DefaultScheme = "Cookies";
+            //options.DefaultChallengeScheme = "oidc";
+            //options.DefaultSignOutScheme = "oidc";
+            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = "oidc";
+            options.DefaultSignOutScheme = "oidc";
+        })
+                    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddJwtBearer("Bearer", options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.Authority = configuration["ZIRALINK_URL_IDS"]!;
+                        options.SaveToken = true;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateAudience = false
+                        };
 
-    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
+                        var db = connectionMultiplexer.GetDatabase(10);
+
+                        options.Events = new JwtBearerEvents
+                        {
+                            OnMessageReceived = async context =>
+                            {
+                                var tokenp = context.Request.Headers["Authorization"];
+                                tokenp = tokenp.ToString().Replace("Bearer ", "");
+                                var token = await db.StringGetAsync($"tokenptoken:{tokenp}");
+                                context.Token = token;
+                            }
+                        };
+                    })
+                    .AddOpenIdConnect("oidc", options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                        options.Authority = configuration["ZIRALINK_URL_IDS"]!;
+                        options.ClientId = "bff";
+                        options.ClientSecret = "secret";
+                        options.ResponseType = OidcConstants.ResponseTypes.Code;
+                        options.Scope.Clear();
+                        options.Scope.Add("ziralink");
+                        options.Scope.Add("openid");
+                        options.Scope.Add("profile");
+                        options.Scope.Add("email");
+                        options.SaveTokens = true;
+
+                        options.GetClaimsFromUserInfoEndpoint = true;
+
+                        if (configuration["ASPNETCORE_ENVIRONMENT"] != "Production")
+                        {
+                            HttpClientHandler handler = new HttpClientHandler();
+                            //handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+                            options.BackchannelHttpHandler = handler;
+                        }
+                    });
+
+        services.AddHttpContextAccessor();
+
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(setup =>
+        {
+            var jwtSecurityScheme = new OpenApiSecurityScheme
+            {
+                BearerFormat = "JWT",
+                Name = "JWT Authentication",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+
+                Reference = new OpenApiReference
+                {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+
+            setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+            setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
         { jwtSecurityScheme, Array.Empty<string>() }
-    });
-});
+            });
+        });
 
     }
 }
