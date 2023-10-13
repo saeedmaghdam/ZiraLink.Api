cbc124335fbfd51f8150cbb747b78ddf9dc54ce1
saeedmaghdam <smasafat@gmail.com>
2023-10-13 08:07:33 +0200

    added new certificates

--- src/ZiraLink.Api/HostingExtensions/ApplicationExtensions.cs
+++ src/ZiraLink.Api/HostingExtensions/ApplicationExtensions.cs
@@ -37,7 +37,7 @@ namespace ZiraLink.Api.HostingExtensions
                     ViewId = Guid.NewGuid(),
                     Title = "Weather Forecasts",
                     State = Domain.Enums.ProjectState.Active,
-                    InternalUrl = "https://host.docker.internal:10051",
+                    InternalUrl = "https://swa.localdev.me:10051",
                     Customer = customer,
                     DateCreated = DateTime.Now,
                     DateUpdated = DateTime.Now,
